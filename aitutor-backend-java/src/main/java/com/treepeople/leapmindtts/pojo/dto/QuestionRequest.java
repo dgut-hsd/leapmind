e61bbe61b8e59017a2d9e274f2f06f8df2f9ba06
tumoshaonian <2429534925@qq.com>
2026-07-23 20:49:40 +0800

    Revert "feat: add all files"
    This reverts commit e1272cb54d53fc1a31f2fae17550afb7e5bc8e1b.

--- aitutor-backend-java/src/main/java/com/treepeople/leapmindtts/pojo/dto/QuestionRequest.java
+++
@@ -1,9 +0,0 @@
-package com.treepeople.leapmindtts.pojo.dto;
-
-import lombok.Data;
-
-@Data
-public class QuestionRequest {
-    private String question;
-    private String userId; // Optional, for cases where it's passed in body
-}
