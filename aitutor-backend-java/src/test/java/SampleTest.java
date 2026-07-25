97975040e4c7e567be9ef84e7dc2e0978f7d06e8
lkq-guji <3048669113@qq.com>
2026-07-26 00:24:23 +0800

    Add M1 frontend and wrong question compatibility API

--- aitutor-backend-java/src/test/java/SampleTest.java
+++ aitutor-backend-java/src/test/java/SampleTest.java
@@ -1,4 +1,5 @@
 
+import com.treepeople.leapmindtts.LeapMindTtsApplication;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @ Description:
  * @ Date：2025/11/3  20:29
  */
-@SpringBootTest
+@SpringBootTest(classes = LeapMindTtsApplication.class, properties = "spring.flyway.enabled=false")
 @Slf4j
 public class SampleTest {
 
